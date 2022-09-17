5a622f5e1c7c3bb73e557d05a1cfd468bba7ffba
BonyChops <contact@bonychops.com>
2022-09-18 01:09:31 +0900

    Edit: entry point

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,38 @@
 const path = require('path');
 
 const baseConfig = {
-  mode: 'production',
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  module: {
-    rules: [{ test: /\.ts$/, loader: 'ts-loader' }],
-  },
+    mode: 'production',
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    module: {
+        rules: [{ test: /\.ts$/, loader: 'ts-loader' }],
+    },
 };
 
 const webConfig = {
-  target: 'web',
-  entry: './src/index.ts',
-  output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'dist'),
-    libraryTarget: 'umd',
-    globalObject: "typeof self !== 'undefined' ? self : this",
-  },
+    target: 'web',
+    entry: './src/index.ts',
+    output: {
+        filename: 'spacha.js',
+        path: path.resolve(__dirname, 'dist'),
+        libraryTarget: 'umd',
+        globalObject: "typeof self !== 'undefined' ? self : this",
+    },
 };
 
 const nodeConfig = {
-  target: 'node',
-  entry: './src/index.node.ts',
-  output: {
-    filename: 'index.node.js',
-    libraryTarget: 'umd',
-    globalObject: "typeof self !== 'undefined' ? self : this",
-    path: path.resolve(__dirname, 'dist'),
-  },
+    target: 'node',
+    entry: './src/index.node.ts',
+    output: {
+        filename: 'spacha.node.js',
+        libraryTarget: 'umd',
+        globalObject: "typeof self !== 'undefined' ? self : this",
+        path: path.resolve(__dirname, 'dist'),
+    },
 };
 
 module.exports = [
-  { ...baseConfig, ...webConfig },
-  { ...baseConfig, ...nodeConfig },
+    { ...baseConfig, ...webConfig },
+    { ...baseConfig, ...nodeConfig },
 ];
